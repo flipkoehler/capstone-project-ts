8f0380abf75c3fcd1a9ec9ba55dc26b3daa95dbf
Philip Koehler <flip.koehler@googlemail.com>
2023-01-03 14:46:20 +0100

    add a for of loop to the quiz logic to compare the arrays

--- components/Quiz/quizLogic.js
+++ components/Quiz/quizLogic.js
@@ -4,7 +4,6 @@ import useFetch from "../../lib/fetch";
 import randomMoviePicker from "../../lib/randomMoviePicker";
 import QuizSteps from "./QuizSteps";
 import { questionAnswer } from "../../assets/data/questionData";
-import { validateHeaderValue } from "http";
 
 export default function MovieQuiz() {
   const movieData = useFetch("/api");
@@ -26,38 +25,69 @@ export default function MovieQuiz() {
   // it is called when the User hits the "Zur Filmempfehlung" Button
   function handleMovieData(updatedQuestionAnswer) {
     // this calculates the min/max values for the runtime
-    const minRuntime = updatedQuestionAnswer[0].answerOptions.find((answer) =>
-      answer.checked ? answer : undefined
+    const minRuntime = updatedQuestionAnswer[0].answerOptions.find(
+      (answer) => answer.checked && answer
     ).value.min;
 
     const maxRuntime = updatedQuestionAnswer[0].answerOptions.findLast(
-      (answer) => (answer.checked ? answer : undefined)
+      (answer) => answer.checked && answer
     ).value.max;
 
     // this calculates the min/max values for the release date
     const minReleaseDate = updatedQuestionAnswer[1].answerOptions.find(
-      (answer) => (answer.checked ? answer : undefined)
+      (answer) => answer.checked && answer
     ).value.min;
 
     const maxReleaseDate = updatedQuestionAnswer[1].answerOptions.findLast(
-      (answer) => (answer.checked ? answer : undefined)
+      (answer) => answer.checked && answer
     ).value.max;
 
-    //step 1: filter through the array and create an new one "filteredMovies" only including movies with the correct duration
-    const filteredMovies = movieDataWithEditedReleaseYear.filter((movie) => {
-      return (
-        movie.runtime >= minRuntime &&
-        movie.runtime <= maxRuntime &&
-        movie.release_date >= minReleaseDate &&
-        movie.release_date <= maxReleaseDate
-      );
-    });
+    const checkedMoods = updatedQuestionAnswer[2].answerOptions
+      .filter((answer) => answer.checked && answer)
+      .map((a) => a.value)
+      .reduce((a, b) => [...a, ...b], []);
+
+    console.log("checkedMoods", checkedMoods);
+
+    //step 1: filter through the array and create an new one "filteredMovies" only including movies with the correct duration and release date
+    const filteredMovies = movieDataWithEditedReleaseYear
+      .filter((movie) => {
+        return (
+          movie.runtime >= minRuntime &&
+          movie.runtime <= maxRuntime &&
+          movie.release_date >= minReleaseDate &&
+          movie.release_date <= maxReleaseDate
+        );
+      })
+      .filter((movie) => movie.mood && movie);
+
+    console.log("filteredmovies", filteredMovies);
+
+    function handleIncludedMoods(checkedMoods, filteredMovies) {
+      for (const mood of checkedMoods) {
+        const filteredMoviesWithMood = filteredMovies.map((movie) => {
+          if (movie.mood.includes(mood)) {
+            return movie;
+          } else {
+            return "no-movie";
+          }
+        });
+        return filteredMoviesWithMood;
+      }
+    }
+
+    const filteredMoviesWithMood = handleIncludedMoods(
+      checkedMoods,
+      filteredMovies
+    ).filter((movie) => movie !== "no-movie");
+
+    console.log("filteredMoviesWithMood", filteredMoviesWithMood);
 
     // Step 2: shuffles a random movie based on the array
-    const randomMovie = randomMoviePicker(filteredMovies);
+    const randomMovie = randomMoviePicker(filteredMoviesWithMood);
 
     // step 3: navigate to the random movie detail page
-    if (filteredMovies.length > 0) {
+    if (filteredMoviesWithMood.length > 0) {
       router.push(`/movies/${randomMovie.id}`);
     } else router.push(`/errorpages/no-movie`);
   }
