1e92ba2dc95a3fcfbbb9919b336ba1dbed158d16
Philip Koehler <flip.koehler@googlemail.com>
2023-01-04 17:15:11 +0100

    add final movie data

--- assets/data/moods.js
+++ assets/data/moods.js
@@ -2,10 +2,6 @@
 
 export const movieMood = [
   {
-    id: 1,
-    value: "interessant",
-  },
-  {
     id: 2,
     value: "lustig",
   },
@@ -54,20 +50,8 @@ export const movieMood = [
     value: "herzergreifend",
   },
   {
-    id: 14,
-    value: "sentimental",
-  },
-  {
-    id: 15,
-    value: "sentimental",
-  },
-  {
     id: 16,
     value: "bezaubernd",
-  },
-  {
-    id: 17,
-    value: "Schmetterlingsgefühle",
   },
   {
     id: 18,
@@ -110,34 +94,31 @@ export const movieMood = [
     value: "gemütlich",
   },
   {
-    id: 27,
+    id: 28,
     value: "ruhig",
   },
+  {
+    id: 29,
+    value: "mind blowing",
+  },
+  {
+    id: 30,
+    value: "neue Sichtweise",
+  },
+  {
+    id: 31,
+    value: "gruselig",
+  },
+  {
+    id: 32,
+    value: "spannend",
+  },
+  {
+    id: 33,
+    value: "unruhig",
+  },
+  {
+    id: 34,
+    value: "aufmunternd",
+  },
 ];
-// 19: "bewegend",
-// 20: "traurig",
-// 21: "ernst",
-// 22: "fordernd",
-// 24: "brutal",
-// 25: "düster",
-// 26: "inspirierend",
-// 27: "hautnah",
-// 29: "gemütlich",
-// 30: "ruhig",
-// 2: "lustig",
-// 3: "schräg",
-// 4: "rau",
-// 5: "explosiv",
-// 6: "aufregend",
-// 7: "bittersüß",
-// 8: "unterhaltsam",
-// 9: "mitreißend",
-// 10: "liebevoll",
-// 11: "glücklich",
-// 12: "romantisch",
-// 13: "aufmunternd",
-// 14: "herzergreifend",
-// 15: "sentimental",
-// 16: "bezaubernd",
-// 17: "Schmetterlingsgefühle",
-// 18: "motivierend",
