fb235db16598b97db7e893b74a7190a02e5c9c02
Philip Koehler <flip.koehler@googlemail.com>
2023-01-06 10:55:36 +0100

    add some sweet styling to the slider

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -1,6 +1,7 @@
 import useEmblaCarousel, { EmblaOptionsType } from "embla-carousel-react";
 import { useCallback } from "react";
 import styled from "styled-components";
+import Link from "next/link";
 
 type PropType = {
   options?: EmblaOptionsType;
@@ -31,16 +32,36 @@ export default function Slider() {
       <StyledSliderContainerDiv ref={emblaRef}>
         <StyledSliderContainerParentDiv>
           <StyledSliderItemDiv>
-            1 Beantworte das Quiz in 4 einfachen FragenBeantworte das Quiz in 4
-            einfachen Fragen
+            <StyledHeadlineDiv>
+              <StyledNumberDiv>1</StyledNumberDiv>
+              <h3>Das Quiz</h3>
+            </StyledHeadlineDiv>
+            <p>
+              Beantworte vier kurze Fragen, nach deiner aktuellen Stimmung. Auf
+              dieser Basis wird dir dann ein Film empfohlen
+            </p>
+            <StyledLink href="/quiz">Quiz jetzt starten!</StyledLink>
           </StyledSliderItemDiv>
           <StyledSliderItemDiv>
-            2 Beantworte das Quiz in 4 einfachen FragenBeantworte das Quiz in 4
-            einfachen Fragen
+            <StyledHeadlineDiv>
+              <StyledNumberDiv>2</StyledNumberDiv>
+              <h3>Zufälliger Film</h3>
+            </StyledHeadlineDiv>
+            <p>Du hast keine Lust auf die Fragen und willst sofort starten?</p>
+            <StyledLink href="/movie-recommendation">
+              Direkt zum Film
+            </StyledLink>
           </StyledSliderItemDiv>
           <StyledSliderItemDiv>
-            3 Beantworte das Quiz in 4 einfachen FragenBeantworte das Quiz in 4
-            einfachen Fragen
+            <StyledHeadlineDiv>
+              <StyledNumberDiv>3</StyledNumberDiv>
+              <h3>Ein Film mit</h3>
+            </StyledHeadlineDiv>
+            <p>
+              Manchmal hat man einfach Lust auf einen Film mit Kevin Costner.
+              Oder vielleicht Tom Hanks?!
+            </p>
+            <h3>folgt bald!</h3>
           </StyledSliderItemDiv>
         </StyledSliderContainerParentDiv>
       </StyledSliderContainerDiv>
@@ -62,14 +83,17 @@ const StyledSliderContainerParentDiv = styled.div`
 
 const StyledSliderItemDiv = styled.div`
   flex: 0 0 44%;
+  flex-wrap: wrap;
   min-width: 0;
   max-width: 100%;
-  background-color: lightblue;
-  height: 200px;
-  margin-right: 1.2rem;
+  background-color: var(--globalWhite);
+  border-radius: 30px 30px 0px 30px;
 
+  margin-right: 1.2rem;
+  padding: 2rem;
   @media screen and (max-width: 600px) {
-    flex: 0 0 100%;
+    flex: 0 0 92%;
+    margin: 0 1rem 0 1rem;
   }
 `;
 
@@ -95,5 +119,38 @@ const StyledButton = styled.button`
 const StyledControlsDiv = styled.div`
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const StyledHeadlineDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+
+const StyledNumberDiv = styled.div`
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 0.2rem solid var(--darkBlue);
+  font-size: 1.3rem;
+  margin-right: 1rem;
+`;
+
+const StyledLink = styled(Link)`
+  max-width: 20rem;
+  max-height: 3rem;
+  border: 0.2rem solid var(--darkBlue);
+  color: var(--smokey-black);
+  margin: 1rem 1rem 1rem 0;
+  padding: 20px;
+  border-radius: 15px;
+  text-align: center;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
   justify-content: center;
 `;
