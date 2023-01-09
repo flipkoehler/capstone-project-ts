0983174d3010c4bedd2d9bc889a1883da34dc7b2
Philip Koehler <flip.koehler@googlemail.com>
2023-01-09 08:58:15 +0100

    start styling the quiz page

--- components/Quiz/QuizSteps.js
+++ components/Quiz/QuizSteps.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
 import { movieMood } from "../../assets/data/moods";
+import Image from "next/image";
 
 export default function QuizSteps({
   currentQuestion,
@@ -21,72 +22,120 @@ export default function QuizSteps({
     });
   };
 
+  const quizBreakpoint = 720;
+
   return (
     <form>
-      <section>
+      <StyledHeadlineParentDiv>
+        <StyledPaginationDiv>Frage {currentStep + 1} von 4</StyledPaginationDiv>
         <h1>{updateAnswer[currentStep].question}</h1>
-        <p>Frage {currentStep + 1} von 4</p>
-        {updateAnswer[currentStep].answerOptions.map((answer, index) => (
-          <StyledAnswerWrapper
-            key={index}
-            onClick={() => updateState(answer.id)}
-          >
-            <StyledInput
-              type="checkbox"
-              id="check"
-              value={answer.value}
-              checked={answer.checked}
-              readOnly
-            />
-            <StyledAnswer>{answer.answer}</StyledAnswer>
-            {(currentStep === 0) | 1 | 3 && (
-              <StyledSpan>{answer.information}</StyledSpan>
-            )}
-            {currentStep === 2 && (
-              <StyledSpan>
-                {movieMood
-                  .filter((mood) => answer.value.includes(mood.id))
-                  .map((mood, index) => (index ? ", " : "") + mood.value)}
-              </StyledSpan>
-            )}
-          </StyledAnswerWrapper>
-        ))}
-        <div>
-          <button
-            disabled={
-              updateAnswer[currentStep].answerOptions.filter(
-                (answer) => answer.checked === true
-              ).length === 0
-                ? true
-                : false
-            }
-            onClick={(event) => onNext(event, updateAnswer[currentStep])}
-          >
-            {isLastStep ? "Zur Filmempfehlung" : "Weiter"}
-          </button>
-        </div>
-      </section>
+      </StyledHeadlineParentDiv>
+
+      <StyledBottomDiv>
+        <StyledAnswerWrapperParentDiv>
+          {updateAnswer[currentStep].answerOptions.map((answer, index) => (
+            <StyledAnswerWrapper
+              key={index}
+              onClick={() => updateState(answer.id)}
+            >
+              <StyledInput
+                type="checkbox"
+                id="check"
+                value={answer.value}
+                checked={answer.checked}
+                readOnly
+              />
+              <StyledAnswer>{answer.answer}</StyledAnswer>
+              {(currentStep === 0) | 1 | 3 && (
+                <StyledSpan>{answer.information}</StyledSpan>
+              )}
+              {currentStep === 2 && (
+                <StyledSpan>
+                  {movieMood
+                    .filter((mood) => answer.value.includes(mood.id))
+                    .map((mood, index) => (index ? ", " : "") + mood.value)}
+                </StyledSpan>
+              )}
+            </StyledAnswerWrapper>
+          ))}
+        </StyledAnswerWrapperParentDiv>
+        <StyledImageWrappeDiv>
+          <Image
+            src={updateAnswer[currentStep].imagePath}
+            width={250}
+            height={250}
+            alt={updateAnswer[currentStep].question}
+          />
+        </StyledImageWrappeDiv>
+      </StyledBottomDiv>
+
+      <div>
+        <button
+          disabled={
+            updateAnswer[currentStep].answerOptions.filter(
+              (answer) => answer.checked === true
+            ).length === 0
+              ? true
+              : false
+          }
+          onClick={(event) => onNext(event, updateAnswer[currentStep])}
+        >
+          {isLastStep ? "Zur Filmempfehlung" : "Weiter"}
+        </button>
+      </div>
     </form>
   );
 }
 
 // CSS Styling
-const StyledAnswerWrapper = styled.div`
+
+// Headline
+
+const StyledHeadlineParentDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 1rem;
+`;
+
+const StyledPaginationDiv = styled.div`
   width: 100%;
-  margin: 0.2rem auto;
+`;
+
+// Bottom Quiz
+const StyledBottomDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  margin: 0 1rem;
+`;
+
+const StyledAnswerWrapperParentDiv = styled.div`
+  max-width: 65%;
+  flex: 65%;
+`;
+
+const StyledAnswerWrapper = styled.div`
+  margin: 0.2rem;
   padding: 0.4rem;
   display: flex;
   flex-wrap: wrap;
-  flex-direction: row;
-  align-items: center;
-  justify-content: left;
-  background-color: var(--lightGray);
+  flex-direction: column;
   cursor: pointer;
+  background-color: var(--globalWhite);
+`;
+
+const StyledImageWrappeDiv = styled.div`
+  display: flex;
+  flex: 35%;
+  align-self: center;
+  justify-content: center;
 `;
 
 const StyledAnswer = styled.p`
   display: flex;
   width: 80%;
+  height: 20px;
   margin: 0 0 0 1rem;
 `;
 
